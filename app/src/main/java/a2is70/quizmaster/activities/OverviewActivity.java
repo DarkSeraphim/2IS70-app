44a31acb5c483845cddb3e0c6011677c2a961f51
Thijs Meeuwisse <thijs@twebber.nl>
2017-04-05 11:12:39 +0200

    OverviewActivity UX improvements
    OverviewActivity now has app bar actions to navigate to groups and to
    sort the quizzes. Also empty state is stated explicitly

--- app/src/main/java/a2is70/quizmaster/activities/OverviewActivity.java
+++ app/src/main/java/a2is70/quizmaster/activities/OverviewActivity.java
@@ -3,12 +3,23 @@ package a2is70.quizmaster.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
 
 import a2is70.quizmaster.R;
 
 public class OverviewActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private TextView emptyView;
+    private List quizzes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,49 @@ public class OverviewActivity extends AppCompatActivity {
                 startActivity(new Intent(OverviewActivity.this, CreateActivity.class));
             }
         });
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.overview_quizzes);
+        emptyView = (TextView) findViewById(R.id.empty_view);
+
+        if (quizzes == null || quizzes.isEmpty()) {
+            recyclerView.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
+        }
+        else {
+            recyclerView.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.overview, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_groups:
+                // User chose the "Settings" item, show the app settings UI...
+                Intent intent = new Intent(this, GroupActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.action_sort_date:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     /*TODO pass Quiz object to QuizActivity through intents
