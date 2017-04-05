5327d5b2c7e36da7d5ebac3676ff818f1866b47d
Jaspurr <jaspersch@hotmail.com>
2017-04-05 12:38:32 +0200

    gimme this ty

--- app/src/main/java/a2is70/quizmaster/data/TeacherReview.java
+++ app/src/main/java/a2is70/quizmaster/data/TeacherReview.java
@@ -7,13 +7,27 @@ public class TeacherReview {
         if (true) throw new UnsupportedOperationException("Booh, you didn't fix me");
     }
 
-    public List<Question> gimmeQuestionPls(){
+    public List<Question> gimmeQuestionsPls(){
         //TODO: insert something to give me a list of questions
         return null;
     }
 
     public int[] gimmeCompletionRatesPls(){
-        //TODO: insert something to give me an of completion rates
+        //TODO: insert something to give me an array of completion rates
         return null;
+    }
+
+    public int gimmeMaxPls(){
+        //TODO: give me the highest grade that has been achieved in this test
+        return 10;
+    }
+    public int gimmeMinPls(){
+        //TODO: give me the lowest grade that has been achieved in this test
+        return 0;
+    }
+
+    public int gimmeAveragePls(){
+        //TODO: give me the average grade of this test
+        return 5;
     }
 }
