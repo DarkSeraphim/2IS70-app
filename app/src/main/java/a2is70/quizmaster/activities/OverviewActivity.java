7e01295abb278972a81f900dfd5505fd043b4ad4
Thijs Meeuwisse <thijs@twebber.nl>
2017-04-05 13:04:02 +0200

    Fill RecyclerView in OverviewActivity with actual quizzes

--- app/src/main/java/a2is70/quizmaster/activities/OverviewActivity.java
+++ app/src/main/java/a2is70/quizmaster/activities/OverviewActivity.java
@@ -1,13 +1,17 @@
 package a2is70.quizmaster.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,11 +22,15 @@ import a2is70.quizmaster.R;
 import a2is70.quizmaster.activities.adapters.DerpData;
 import a2is70.quizmaster.data.Account;
 import a2is70.quizmaster.data.AppContext;
+import a2is70.quizmaster.data.Group;
 import a2is70.quizmaster.data.Quiz;
 
 public class OverviewActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     private TextView emptyView;
     private List<Quiz> quizzes;
 
@@ -57,17 +65,40 @@ public class OverviewActivity extends AppCompatActivity {
         }
 
 
-        recyclerView = (RecyclerView) findViewById(R.id.overview_quizzes);
+        mRecyclerView = (RecyclerView) findViewById(R.id.overview_quizzes);
         emptyView = (TextView) findViewById(R.id.empty_view);
 
         if (quizzes == null || quizzes.isEmpty()) {
-            recyclerView.setVisibility(View.GONE);
+            mRecyclerView.setVisibility(View.GONE);
             emptyView.setVisibility(View.VISIBLE);
         }
         else {
-            recyclerView.setVisibility(View.VISIBLE);
+            mRecyclerView.setVisibility(View.VISIBLE);
             emptyView.setVisibility(View.GONE);
         }
+
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+
+        mAdapter = new QuizAdapter(this, quizzes);
+        // Attach the adapter to the recyclerview to populate items
+        mRecyclerView.setAdapter(mAdapter);
+        // Set layout manager to position the items
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
+//
+//
+//
+//        // use a linear layout manager
+//        mLayoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+//
+//        // specify an adapter (see also next example)
+//        mAdapter = new QuizAdapter(quizzes);
+//        mRecyclerView.setAdapter(mAdapter);
 
     }
 
@@ -107,9 +138,102 @@ public class OverviewActivity extends AppCompatActivity {
 
         }
     }
+
     /*TODO pass Quiz object to QuizActivity through intents
     * Intent i = new Intent(getApplicationContext(), QuizActivity.class);
     * i.putExtra("quiz",new Gson().toJson(Quiz Object));
     * startActivity(i);
     */
+}
+
+// Adapter for quiz overview
+class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.ViewHolder> {
+    private List<Quiz> mQuizzes;
+    private Context mContext;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView quizNameView;
+        public TextView quizGroupView;
+        public TextView quizDeadlineView;
+//        public Button messageButton;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            quizNameView = (TextView) itemView.findViewById(R.id.quiz_name);
+            quizGroupView = (TextView) itemView.findViewById(R.id.quiz_group);
+            quizDeadlineView = (TextView) itemView.findViewById(R.id.quiz_deadline);
+//            messageButton = (Button) itemView.findViewById(R.id.message_button);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public QuizAdapter(Context context, List<Quiz> myQuizzes) {
+        mQuizzes = myQuizzes;
+        mContext = context;
+    }
+
+    private Context getContext() {
+        return mContext;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public QuizAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_quiz, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // Get the data model based on position
+        Quiz quiz = mQuizzes.get(position);
+
+        // Set item views based on your views and data model
+        TextView quizNameView = holder.quizNameView;
+        quizNameView.setText(quiz.getName());
+        TextView quizGroupView = holder.quizGroupView;
+
+        Group[] groups = quiz.getGroup();
+        String groupString = "";
+        for(int i = 0; i < groups.length; i++) {
+            groupString += groups[i].getName();
+            if(i < groups.length-1) {
+                groupString += ", ";
+            }
+        }
+        quizGroupView.setText(groupString);
+
+        TextView quizDeadlineView = holder.quizDeadlineView;
+        String deadline = String.valueOf(quiz.getCloseAt());
+        quizDeadlineView.setText(deadline);
+
+
+//        Button button = viewHolder.messageButton;
+//        button.setText("Message");
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mQuizzes.size();
+    }
 }
