d6b79216533b0dbc9bda33f69901828afe2d84ef
Jaspurr <jaspersch@hotmail.com>
2017-04-05 11:16:14 +0200

    Hijacked your button

--- app/src/main/java/a2is70/quizmaster/activities/OverviewActivity.java
+++ app/src/main/java/a2is70/quizmaster/activities/OverviewActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 
 import a2is70.quizmaster.R;
+import a2is70.quizmaster.data.Account;
+import a2is70.quizmaster.data.AppContext;
 
 public class OverviewActivity extends AppCompatActivity {
 
@@ -15,15 +17,25 @@ public class OverviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
 
-        Button mAddQuizButton = (Button) findViewById(R.id.overview_add_quiz);
-        mAddQuizButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(OverviewActivity.this, CreateActivity.class));
-            }
-        });
-    }
 
+        Button mAddQuizButton = (Button) findViewById(R.id.overview_add_quiz);
+        if (AppContext.getInstance().getAccount().getType() == Account.Type.TEACHER) {
+            mAddQuizButton.setText("%$#{HACKED}%#$@%");
+            mAddQuizButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(OverviewActivity.this, ReviewActivity.class));
+                }
+            });
+        } else {
+            mAddQuizButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(OverviewActivity.this, CreateActivity.class));
+                }
+            });
+        }
+    }
     /*TODO pass Quiz object to QuizActivity through intents
     * Intent i = new Intent(getApplicationContext(), QuizActivity.class);
     * i.putExtra("quiz",new Gson().toJson(Quiz Object));
